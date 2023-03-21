89c122a3ab7629282e938e8ce23ca66221f1a1b1
mayank0591560319 <108619585+mayank0591560319@users.noreply.github.com>
2023-03-21 13:43:37 +0530

    Initial commit

---
+++ app/src/main/java/com/mayank/tictactoegame/MainActivity.java
@@ -0,0 +1,88 @@
+package com.mayank.tictactoegame;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    boolean gameActive = true;
+    //0-X
+    //1-0
+    int activePlayer = 0;
+    int[] gameState = {2, 2, 2, 2, 2, 2, 2, 2, 2};
+    //player representation
+    //1=0
+    //0=X
+    //2=null
+    int[][] winningPositions = {{0, 1, 2}, {3, 4, 5}, {6, 7, 8},
+            {0, 3, 6}, {1, 4, 7}, {2, 5, 8},
+            {0, 4, 8}, {2, 4, 6}};
+    private Object View;
+
+
+    public void playerTap(View view) {
+        ImageView img = (ImageView) view;
+        int tappedImage = Integer.parseInt(img.getTag().toString());
+        if (!gameActive) {
+            gameReset((android.view.View) View);
+        }
+        if (gameState[tappedImage] == 2) {
+            gameState[tappedImage] = activePlayer;
+            img.setTranslationY(-1000f);
+            if (activePlayer == 0) {
+                img.setImageResource(R.drawable.x);
+                activePlayer = 1;
+                TextView status = findViewById(R.id.status);
+                status.setText("O's turn -tap to play");
+            } else {
+                activePlayer = 0;
+                TextView status = findViewById(R.id.status);
+                status.setText("X's turn-tap to play");
+            }
+        }
+        img.animate().translationYBy(1000f).setDuration(300);
+//Check if any player has won
+        for (int[] winPosition : winningPositions) {
+            if (gameState[winPosition[0]] == gameState[winPosition[1]] &&
+                    gameState[winPosition[1]] == gameState[winPosition[2]] &&
+                    gameState[winPosition[0]] != 2) {
+                //Somebody has won! find
+                String winnerStr;
+                if (gameState[winPosition[0]] == 0) {
+                    winnerStr = "X has won";
+                } else {
+                    winnerStr = "O has won";
+                }
+                //Update the status for winner announcement
+                TextView status = findViewById(R.id.status);
+                status.setText(winnerStr);
+            }
+        }}
+            public void gameReset(View view){
+                gameActive = true;
+                activePlayer = 0;
+                for (int i = 0; i < gameState.length; i++) {
+                    gameState[i] = 2;
+                }
+                ((ImageView) findViewById(R.id.imageView0)).setImageResource(0);
+                ((ImageView) findViewById(R.id.imageView1)).setImageResource(0);
+                ((ImageView) findViewById(R.id.imageView2)).setImageResource(0);
+                ((ImageView) findViewById(R.id.imageView3)).setImageResource(0);
+                ((ImageView) findViewById(R.id.imageview4)).setImageResource(0);
+                ((ImageView) findViewById(R.id.imageView5)).setImageResource(0);
+                ((ImageView) findViewById(R.id.imageview6)).setImageResource(0);
+                ((ImageView) findViewById(R.id.imageView7)).setImageResource(0);
+                ((ImageView) findViewById(R.id.imageView8)).setImageResource(0);
+
+            }
+
+            @Override
+            protected void onCreate (Bundle savedInstanceState){
+                super.onCreate(savedInstanceState);
+                setContentView(R.layout.activity_main);
+            }
+        }
+
